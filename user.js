bd2978f2cd9ec52cfeb9d3ece01b32065d435b14
Bilal Mehfooz <93863145+BilalMehfooz12@users.noreply.github.com>
2022-06-01 11:28:17 +0500

    Add files via upload

---
+++ user.js
@@ -0,0 +1,14 @@
+export default function fu()
+{
+    console.log("Bilal Hussain")
+}
+export function func()
+{
+    console.log("Aliyan Ali")
+}
+export class data{
+    read()
+    {
+        console.log("Export The Class")
+    }
+}
